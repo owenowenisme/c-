532ad9cd293530d0e61675964ccc8c4cbc77df4b
owenowenisme <mses010108@gmail.com>
2020-12-03 14:50:16 +0800

    ddd

---
+++ main.cpp
@@ -0,0 +1,35 @@
+#include<map>
+#include<set>
+#include<stack>
+#include<queue>
+#include<vector>
+#include<algorithm>
+#include<functional>
+#include<iterator>
+#include<iostream>
+#include "bits/stdc++.h"
+
+#define io ios_base::sync_with_stdio(false);cin.tie(0);
+#define mem(a, val) memset(a,val,sizeof(a))
+#define pb push_back
+using namespace std;
+
+void solu() {
+}
+
+int main(void) {
+    int t;cin>>t;
+    while(t--){
+        int n;cin>>n;int temp;int cnt=0;int flag=0;
+        for(int i=0;i<n;i++){
+            cin>>temp;
+            if(flag==0&&temp==1){
+                flag=1;
+            }
+        }
+    }
+    return 0;
+}
+
+
+
